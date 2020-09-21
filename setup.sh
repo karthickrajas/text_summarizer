627945e980dd2a2e8432d5408301616ee552722f
Raja, Karthick <karthick.raja@philips.com>
2020-09-21 14:53:36 +0530

    debugging setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
 mkdir -p ~/.streamlit/
 
-echo "\
-[general]\n\
-email = \"your-email@domain.com\"\n\
+echo "[general]
+email = \"karthick11b36@gmai.com\"
 " > ~/.streamlit/credentials.toml
 
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
